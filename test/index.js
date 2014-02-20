a30b856cc3a1d685badc1830aa15be25b7c929c5
Forbes Lindesay <forbes@lindesay.co.uk>
2014-02-20 16:48:41 +0000

    Fix test

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var test = require('testit');
 var compile = require('../');
 
 test('compile twbs', function () {
-  return compile.toFolder(__dirname + '/fixtures/twbs.less', __dirname + '/output/twbs.css').then(function (res) {
+  return compile.toDisc(__dirname + '/fixtures/twbs.less', __dirname + '/output/twbs.css').then(function (res) {
     console.dir(res.files);
   });
 });
